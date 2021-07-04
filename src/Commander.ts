50602160f2016f783dc1696ec0f3e709f7dc00f2
jvals <jvals@users.noreply.github.com>
2021-07-04 18:54:29 +0200

    Remove property _suggestedPrestige

--- src/Commander.ts
+++ src/Commander.ts
@@ -4,7 +4,6 @@ class Commander {
     private readonly _prestiges: Prestige[];
     readonly name: string;
     private readonly _shortName: string
-    private _suggestedPrestige: Prestige | undefined;
     constructor(name: string, prestiges: Prestige[], _shortName: string | undefined = undefined) {
         this.name = name;
         this._prestiges = prestiges;
@@ -12,22 +11,16 @@ class Commander {
     }
 
     get suggestedPrestige(): Prestige {
-        return this._suggestedPrestige ?? this.updateAndGetRandomPrestige();
-    }
-
-    set suggestedPrestige(value: Prestige) {
-        this._suggestedPrestige = value;
+        return this.getRandomPrestige();
     }
 
     getLinkToSC2Coop() {
         return `https://starcraft2coop.com/commanders/${this._shortName.toLowerCase()}#buildOrder`
     }
 
-    private updateAndGetRandomPrestige(): Prestige {
+    private getRandomPrestige(): Prestige {
         const idx = Math.floor(Math.random()*this._prestiges.length);
-        const randomPrestige = this._prestiges[idx];
-        this._suggestedPrestige =  randomPrestige;
-        return randomPrestige;
+        return this._prestiges[idx];
     }
 
 }
