ee666c61480d9d76e9263247a52e7d481ac0ce12
jvals <jvals@users.noreply.github.com>
2022-07-27 21:22:02 +0200

    Add a grid layout to the settings page

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -17,24 +17,29 @@
 
 <h2>Settings</h2>
 <p class="notice">Settings are saved in the browser local storage. Click <button class="reset-button" on:click={handleReset}>here</button> to reset.</p>
-{#each commanders as commander}
-    <div class="commander-settings">
-        <input type="checkbox" id={commander.name} bind:checked={commander.isAvailable}>
-        <label for={commander.name}>{commander.name}</label>
-        <div class="prestige-settings">
-            {#each commander.prestiges as prestige}
-                <div>
-                    <input type="checkbox" id={prestige.name} bind:checked={prestige.isAvailable} disabled={!commander.isAvailable}>
-                    <label for={prestige.name}>{prestige.name}</label>
+<div class="settings-grid">
+    {#each commanders as commander}
+            <div class="commander-settings">
+                <input type="checkbox" id={commander.name} bind:checked={commander.isAvailable}>
+                <label for={commander.name}>{commander.name}</label>
+                <div class="prestige-settings">
+                    {#each commander.prestiges as prestige}
+                        <div>
+                            <input type="checkbox" id={prestige.name} bind:checked={prestige.isAvailable} disabled={!commander.isAvailable}>
+                            <label for={prestige.name}>{prestige.name}</label>
+                        </div>
+                    {/each}
                 </div>
-            {/each}
-        </div>
-    </div>
-{/each}
+            </div>
+    {/each}
+</div>
 
 <style>
  .commander-settings {
-     margin-left: 16px;
+     margin: 16px;
+     padding: 16px;
+     border: solid #006699;
+     border-radius: 16px;
  }
  .prestige-settings {
      margin-left: 32px;
@@ -52,5 +57,9 @@
      color: #069;
      text-decoration: underline;
      cursor: pointer;
+ }
+ .settings-grid {
+     display: grid;
+     grid-template-columns: repeat(3, 1fr);
  }
 </style>
